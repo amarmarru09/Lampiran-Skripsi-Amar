3c177b57b4bcea6b2579dd493714d42e6b07baff
amarmarru09 <marufamar022@gmail.com>
2026-01-03 16:57:16 +0800

    Create redirect_test.js

---
+++ configurasi/pengujian/redirect_test.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 50 },
+    { duration: '30s', target: 100 },
+    { duration: '30s', target: 200 },
+    { duration: '30s', target: 0 },
+  ],
+};
+
+export default function () {
+  const url = 'http://IP_SERVER_UTAMA/';
+  const res = http.get(url, { redirects: 0 }); // penting: redirect dimatiin
+
+  check(res, {
+    'status is 200 (normal)': (r) => r.status === 200,
+    'redirect detected (301/302)': (r) =>
+      r.status === 301 || r.status === 302,
+  });
+
+  sleep(1);
+}
