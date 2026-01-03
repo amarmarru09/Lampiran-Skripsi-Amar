33cac1b599d6969cc545a0bbb96df573dcf46e5c
amarmarru09 <marufamar022@gmail.com>
2026-01-03 15:50:46 +0800

    Update auto-redirect.sh

--- auto-redirect.sh
+++ auto-redirect.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+ALERT="/var/log/snort/snort.alert.fast"
+BACKUP_IP="192.168.1.21"
+
+tail -Fn0 $ALERT | while read line; do
+  if echo "$line" | grep -i "DoS\|DDoS\|SYN"; then
+    IP=$(echo $line | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -n 1)
+
+    if [ ! -z "$IP" ]; then
+      iptables -t nat -C PREROUTING -s $IP -p tcp --dport 80 -j DNAT --to-destination $BACKUP_IP 2>/d>
+      iptables -t nat -A PREROUTING -s $IP -p tcp --dport 80 -j DNAT --to-destination $BACKUP_IP
+
+      echo "$(date) - Redirect attacker $IP to $BACKUP_IP" >> /var/log/auto-redirect.log
+    fi
+  fi
+done
